90473011d0cea5a5b1e0abce0c3c6554f970f71f
shigeyukey <shigge.3.21@gmail.com>
2024-11-16 04:07:53 +0900

    update

--- HTML/GenerateHTML.py
+++ HTML/GenerateHTML.py
@@ -436,7 +436,7 @@ buttons = [
     "description": "(Free, Custom) Visualize reviewed cards and remaining",
     "link": "https://shigeyukey.github.io/shige-addons-wiki/progress-bar.html"},
 
-    {"url": "https://raw.githubusercontent.com/shigeyukey/shige-addons/main/addons_media/patreon_progress_bar_for_Anki_how_to_use.webp",
+    {"url": "https://raw.githubusercontent.com/shigeyukey/shige-addons/main/addons_media/patreon_chunk_progressbar.webp",
     "label": "⌛️Chunk Progressbar",
     "description": "(Patron) Progress bars for chunking Anki cards.",
     "link": "https://shigeyukey.github.io/shige-addons-wiki/progress-bar-for-anki.html"},
